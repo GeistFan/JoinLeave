668fef54da36206924da1040bbbf52001a5d33e3
GeistFan <48773535+GeistFan@users.noreply.github.com>
2020-08-21 17:23:59 +0200

    Add files via upload

---
+++ src/JoinLeave/Main.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace JoinLeave;
+
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\event;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener
+{
+    public function onEnable()
+    {
+        $this->getlogger()->info("JoinLeave by GeistFan");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+    public function onPlayerJoin(PlayerJoinEvent $event)
+    {
+        $player = $event->getPlayer();
+        $event->setJoinMessage("");
+    }
+
+    public function onPlayerQuit(PlayerQuitEvent $event)
+    {
+        $player = $event->getPlayer();
+        $event->setQuitMessage("");
+    }
+
+}
