5219dd0a838308f53b0db0ac3dbcf9036174740e
GeistFan <48773535+GeistFan@users.noreply.github.com>
2021-05-05 22:47:55 +0200

    Update Main.php

--- src/JoinLeave/Main.php
+++ src/JoinLeave/Main.php
@@ -10,27 +10,36 @@ use pocketmine\event;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 
-class Main extends PluginBase implements Listener
+class Main extends PluginBase implements Listene
 {
+    public $cfg;
+
     public function onEnable()
     {
-        $this->getlogger()->info("JoinLeave by GeistFan and GodWeedZao");
+        $this->getlogger()->info("JoinLeave by GeistFan");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        @mkdir($this->getDataFolder());
+        $this->saveDefaultConfig();
+        $this->getResource("config.yml");
+        
     }
     public function onPlayerJoin(PlayerJoinEvent $event)
     {
-        $player = $event->getPlayer();
-        $event->setJoinMessage("");
+        $join = $this->getConfig()->get("join");
+        $player = $event->getPlayer()->getName();
+        $event->setJoinMessage(str_replace("{name}",$player,$join));
     }
 
     public function onPlayerQuit(PlayerQuitEvent $event)
     {
-        $player = $event->getPlayer();
-        $event->setQuitMessage("");
+        $leave = $this->getConfig()->get("leave");
+        $player = $event->getPlayer()->getName();
+        $event->setQuitMessage(str_replace("{name",$player,$leave)));
     }
 
-    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args, Player $player):bool
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args):bool
     {
         if($cmd->getName() === "tjl")
         {
